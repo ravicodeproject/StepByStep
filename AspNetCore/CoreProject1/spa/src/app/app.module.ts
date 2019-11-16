f2c911586a9b8f2c0409f2bc0b4d4111d0640de8
ravikumar <ravicodeproject@gmail.com>
2019-11-16 19:42:50 +0530

    Basic CoreWebApi with Angular ready

--- AspNetCore/CoreProject1/spa/src/app/app.module.ts
+++ AspNetCore/CoreProject1/spa/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValuesComponent } from './values/values.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValuesComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
